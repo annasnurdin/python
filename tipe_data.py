6c4df462c1f155a52433072b73d32b1e3598de7f
annasnurdin <anasnurdin02@gmail.com>
2026-01-18 20:49:04 +0700

    tipe data python

---
+++ tipe_data.py
@@ -0,0 +1,15 @@
+harga = 100000
+print(harga)
+
+harga = "diskon" # bisa berubah tipe datanya
+print(harga)
+
+price = 100000
+discount = 0.5
+print(discount)
+
+total = price * discount
+print("harga akhir: ", total)
+
+is_discount = True # capital bro
+print(is_discount)
