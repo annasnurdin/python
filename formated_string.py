4cc6573949eb6dee7e6f5190afea89497d32c6b1
annasnurdin <anasnurdin02@gmail.com>
2026-01-20 11:51:11 +0700

    format string

---
+++ formated_string.py
@@ -0,0 +1,8 @@
+first_name = "Annas"
+last_name = "Nurdin"
+
+full_name = first_name + " [" + last_name + "]"
+print(full_name)
+
+full_name = f"{first_name} [{last_name}]" # bisa format seperti ini
+print(full_name)
