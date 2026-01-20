779f858c0f6d050a1ebf5cd24c8dc7198a3892f6
annasnurdin <anasnurdin02@gmail.com>
2026-01-20 13:05:14 +0700

    string method

--- formated_string.py
+++ formated_string.py
@@ -4,6 +4,9 @@ last_name = "Nurdin"
 full_name = first_name + " [" + last_name + "]"
 print(full_name)
 
+uppercase_name = full_name.upper()
+print(uppercase_name)
+
 full_name = f"{first_name} [{last_name}]" # bisa format seperti ini
 print(full_name)
 
