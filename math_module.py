9d62fd8ac06ee689829c780efa400e038f818248
annasnurdin <anasnurdin02@gmail.com>
2026-01-20 13:18:26 +0700

    math

---
+++ math_module.py
@@ -0,0 +1,16 @@
+from math import ceil # import fungsi ceil dari module math
+import math # ini namanya module
+
+
+number = 0.6
+number = round(number) # bisa tanpa import
+
+angka = 0.4
+angka = ceil(angka) # import math (module)
+
+num = 0.3
+num = math.log(num)
+
+print(number)
+print(angka)
+print(num)
