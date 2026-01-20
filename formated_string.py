b004efac6415d882b21e62e7dd0b32aee253e1bb
annasnurdin <anasnurdin02@gmail.com>
2026-01-20 11:52:55 +0700

    auto converted to string

--- formated_string.py
+++ formated_string.py
@@ -6,3 +6,8 @@ print(full_name)
 
 full_name = f"{first_name} [{last_name}]" # bisa format seperti ini
 print(full_name)
+
+umur = 21
+pesan = f"Umur kamu: {umur}" # auto conversion to string
+
+print(pesan)
