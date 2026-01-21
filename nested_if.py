99a8d79d7a1ca91f498a9f83c1ba49b6b4628180
annasnurdin <anasnurdin02@gmail.com>
2026-01-22 05:41:01 +0700

    nested if

---
+++ nested_if.py
@@ -0,0 +1,11 @@
+username = input("Username: ")
+password = input("Password: ")
+
+if username == "Annas":
+  if password == "123":
+    print("Password Benar")
+    print("Hallo")
+  else: 
+    print("Password Salah")
+else:
+  print("Username tidak dikenali")
