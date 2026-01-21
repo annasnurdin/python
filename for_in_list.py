3a29ab093ebd49e98cfce4399b3d3e07e98bea10
annasnurdin <anasnurdin02@gmail.com>
2026-01-22 06:20:34 +0700

    sum for in list

---
+++ for_in_list.py
@@ -0,0 +1,10 @@
+daftar_nama = ["Annas", "Purwo", "Karin"]
+
+for nama in daftar_nama:
+  print(f"Nama: {nama}")
+
+daftar_nilai = [90, 80, 99]
+total = 0
+for nilai in daftar_nilai:
+  total += nilai
+print(total)
