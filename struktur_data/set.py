2518ef155d60b3adfe642889d759d6a68babdf3f
annasnurdin <anasnurdin02@gmail.com>
2026-01-24 19:27:22 +0700

    set (tidak duplikat)

---
+++ struktur_data/set.py
@@ -0,0 +1,7 @@
+persons = {"annas", "purwo", "karin"}
+print(persons)
+
+persons.add("annas")
+print(persons) # tidak bisa duplikat atau harus unik
+persons.remove("annas")
+print(persons) # tidak bisa urut
